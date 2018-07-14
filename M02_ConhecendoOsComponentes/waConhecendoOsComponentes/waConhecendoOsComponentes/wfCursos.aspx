bde391be6568d0b65fa5e3dc71b8e3cf2fa788bf
Fernando Jesus <fernando.js8@outlook.com>
2018-07-14 08:26:13 -0300

    Fim Aula 15 - Seção 2

---
+++ M02_ConhecendoOsComponentes/waConhecendoOsComponentes/waConhecendoOsComponentes/wfCursos.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="wfCursos.aspx.cs" Inherits="waConhecendoOsComponentes.wfCursos" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:MultiView ID="MultiView1" runat="server" ActiveViewIndex="0">
+                <asp:View ID="View1" runat="server">
+                    <asp:Image ID="Image1" runat="server" ImageUrl="~/Imagens/Curso1.jpg" AlternateText="Dota 2 Underhollow" DescriptionUrl="Dota 2 Underhollow" Height="540px" Width="544px" />
+                    <asp:Image ID="Image2" runat="server" AlternateText="Dota 2 Rampage" DescriptionUrl="Dota 2 Rampage" Height="476px" ImageUrl="~/Imagens/Curso2.jpg" style="margin-top: 0px" Width="586px" />
+                    <br />
+                </asp:View>
+                <asp:View ID="View2" runat="server">
+                </asp:View>
+                <asp:View ID="View3" runat="server">
+                </asp:View>
+            </asp:MultiView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ M02_ConhecendoOsComponentes/waConhecendoOsComponentes/waConhecendoOsComponentes/wfCursos.aspx.designer.cs
@@ -0,0 +1,78 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Este código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace waConhecendoOsComponentes {
+    
+    
+    public partial class wfCursos {
+        
+        /// <summary>
+        /// Controle form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Controle MultiView1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.MultiView MultiView1;
+        
+        /// <summary>
+        /// Controle View1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.View View1;
+        
+        /// <summary>
+        /// Controle Image1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image1;
+        
+        /// <summary>
+        /// Controle Image2.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image2;
+        
+        /// <summary>
+        /// Controle View2.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.View View2;
+        
+        /// <summary>
+        /// Controle View3.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.View View3;
+    }
+}
