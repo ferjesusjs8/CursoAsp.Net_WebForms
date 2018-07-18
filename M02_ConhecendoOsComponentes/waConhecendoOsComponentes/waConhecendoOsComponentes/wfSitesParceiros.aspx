ac09848f6f84ba70bd80727151d714ddde0d08bb
Fernando Jesus <fernando.js8@outlook.com>
2018-07-18 20:52:39 -0300

    Fim da Aula 20 - Seção 2

---
+++ M02_ConhecendoOsComponentes/waConhecendoOsComponentes/waConhecendoOsComponentes/wfSitesParceiros.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="wfSitesParceiros.aspx.cs" Inherits="waConhecendoOsComponentes.wfSitesParceiros" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:ImageMap ID="ImageMap1" runat="server" ImageUrl="~/Imagens/Curso1.jpg">
+                <asp:RectangleHotSpot AlternateText="Site Make Games" Bottom="200" NavigateUrl="https://www.google.com/" Right="200" Target="_blank" />
+                <asp:RectangleHotSpot AlternateText="Site 2" Bottom="300" Left="450" NavigateUrl="https://www.google.com/" Right="800" Target="_blank" />
+                <asp:CircleHotSpot AlternateText="Site 3" NavigateUrl="https://www.google.com/" Radius="250" Target="_blank" X="600" Y="500" />
+                <asp:PolygonHotSpot AlternateText="Site Polygon" Coordinates="0,300,200,300,200,400,0,400,0,300" NavigateUrl="https://www.google.com/" Target="_blank" />
+            </asp:ImageMap>
+        </div>
+    </form>
+</body>
+</html>
---
+++ M02_ConhecendoOsComponentes/waConhecendoOsComponentes/waConhecendoOsComponentes/wfSitesParceiros.aspx.designer.cs
@@ -0,0 +1,33 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Este código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace waConhecendoOsComponentes {
+    
+    
+    public partial class wfSitesParceiros {
+        
+        /// <summary>
+        /// Controle form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Controle ImageMap1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageMap ImageMap1;
+    }
+}
